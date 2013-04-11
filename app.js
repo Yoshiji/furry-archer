fa775c57345d3a7e0f9f0800a60da66cb2309308
Yoshi <mathieu.beauche@gmail.com>
2013-04-11 15:00:31 -0400

    ok

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ var express = require('express')
   , http = require('http')
   , fs = require('fs')
   , path = require('path')
-  , mongoose = require('mongoose');
+  , mongoose = require('mongoose')
+  , socket = require('socket.io');
 
 // Loads the /config/config.js
 exports = module.exports = config = require('./config/config.js');
@@ -26,6 +27,22 @@ var app = express();
 require('./config/express.js')(app, config);
 require('./config/routes.js')(app);
 
-http.createServer(app).listen(app.get('port'), function(){
+// Creates the server and listen to the port 3000
+var server = http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
+});
+
+// Creates the socket Input/Output
+var io = socket.listen(server);
+io.sockets.on('connection', function(socket) {
+  
+  console.log("connected");
+  socket.emit("welcome", {txt: "Welcome message from the server"})
+  socket.on('disconnect', function (client) {
+    console.log("disconnected");
+  });
+  
+  socket.on('ping', function (data) {
+    socket.emit("pong",{txt:"Pong (from server)"});
+  });
 });
