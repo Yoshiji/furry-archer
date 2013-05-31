644eac2c819efc0484241412f934afc19e606d97
Matthieu Billard <matthieu@david-goliath.com>
2013-05-31 16:06:08 -0400

    ajout du nouveau sprite

--- public/javascripts/_sprites.js
+++ public/javascripts/_sprites.js
@@ -1,9 +1,71 @@
 Crafty.sprite(128, 148, "images/grasstile.png", {
 	grass: [0,0,2,1],
 	water: [2,0,2,1],
-	my_grass: [0,1,2,1],
-	others_grass: [2,1,2,1],
 	voided: [2,0,2,1]
+	my_grass: [4,0,2,1],
+	others_grass: [6,0,2,1],
+
+	//MY GRASS
+	my_seeded_good: [0,1,2,1],
+	my_seedlings_good: [2,1,2,1],
+	my_little_plants_good: [4,1,2,1],
+	my_plants_good: [6,1,2,1],
+	my_mature_plants_good: [8,1,2,1],
+
+	my_seeded_medium: [0,2,2,1],
+	my_seedlings_medium: [2,2,2,1],
+	my_little_plants_medium: [4,2,2,1],
+	my_plants_medium: [6,2,2,1],
+	my_mature_plants_medium: [8,2,2,1],
+
+	my_seeded_average: [0,3,2,1],
+	my_seedlings_average: [2,3,2,1],
+	my_little_plants_average: [4,3,2,1],
+	my_plants_average: [6,3,2,1],
+	my_mature_plants_average: [8,3,2,1],
+
+	my_seeded_low: [0,4,2,1],
+	my_seedlings_low: [2,4,2,1],
+	my_little_plants_low: [4,4,2,1],
+	my_plants_low: [6,4,2,1],
+	my_mature_plants_low: [8,4,2,1],
+
+	my_seeded_bad: [0,5,2,1],
+	my_seedlings_bad: [2,5,2,1],
+	my_little_plants_bad: [4,5,2,1],
+	my_plants_bad: [6,5,2,1],
+	my_mature_plants_bad: [8,5,2,1],
+
+	// OTHERS GRASS
+	others_seeded_good: [0,6,2,1],
+	others_seedlings_good: [2,6,2,1],
+	others_little_plants_good: [4,6,2,1],
+	others_plants_good: [6,6,2,1],
+	others_mature_plants_good: [8,6,2,1],
+
+	others_seeded_medium: [0,7,2,1],
+	others_seedlings_medium: [2,7,2,1],
+	others_little_plants_medium: [4,7,2,1],
+	others_plants_medium: [6,7,2,1],
+	others_mature_plants_medium: [8,7,2,1],
+
+	others_seeded_average: [0,8,2,1],
+	others_seedlings_average: [2,8,2,1],
+	others_little_plants_average: [4,8,2,1],
+	others_plants_average: [6,8,2,1],
+	others_mature_plants_average: [8,8,2,1],
+
+	others_seeded_low: [0,9,2,1],
+	others_seedlings_low: [2,9,2,1],
+	others_little_plants_low: [4,9,2,1],
+	others_plants_low: [6,9,2,1],
+	others_mature_plants_low: [8,9,2,1],
+
+	others_seeded_bad: [0,10,2,1],
+	others_seedlings_bad: [2,10,2,1],
+	others_little_plants_bad: [4,10,2,1],
+	others_plants_bad: [6,10,2,1],
+	others_mature_plants_bad: [8,10,2,1]
 });
 
 Crafty.sprite(8, 8, "images/players.png", {
